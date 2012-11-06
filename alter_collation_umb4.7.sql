e3ba8837932924b19c405f8faac75acb7f550bd6
Astuanax <len@astuanax.com>
2012-11-06 10:19:41 +0100

    SQL Export fro Umbraco

---
+++ alter_collation_umb4.7.sql
@@ -0,0 +1,17 @@
+ALTER TABLE [dbo].[cmsTemplate] ALTER COLUMN design ntext COLLATE database_default NOT NULL;
+ALTER TABLE [dbo].[umbracoStatSession] ALTER COLUMN visitorId char(36) COLLATE database_default NULL;
+ALTER TABLE [dbo].[umbracoStatSession] ALTER COLUMN browserType nvarchar(510) COLLATE database_default NULL;
+ALTER TABLE [dbo].[umbracoNode] ALTER COLUMN path nvarchar(300) COLLATE database_default NOT NULL;
+ALTER TABLE [dbo].[cmsPropertyType] ALTER COLUMN validationRegExp nvarchar(510) COLLATE database_default NULL;
+ALTER TABLE [dbo].[cmsPropertyType] ALTER COLUMN Description nvarchar(4000) COLLATE database_default NULL;
+ALTER TABLE [dbo].[cmsContentXml] ALTER COLUMN xml ntext COLLATE database_default NOT NULL;
+ALTER TABLE [dbo].[cmsDataTypePreValues] ALTER COLUMN alias nvarchar(100) COLLATE database_default NULL;
+ALTER TABLE [dbo].[cmsDictionary] ALTER COLUMN [key] nvarchar(2000) COLLATE database_default NOT NULL;
+ALTER TABLE [dbo].[cmsLanguageText] ALTER COLUMN value nvarchar(2000) COLLATE database_default NOT NULL;
+ALTER TABLE [dbo].[umbracoRelationType] ALTER COLUMN name nvarchar(510) COLLATE database_default NOT NULL;
+ALTER TABLE [dbo].[umbracoRelationType] ALTER COLUMN alias nvarchar(200) COLLATE database_default NULL;
+ALTER TABLE umbracoUser2NodeNotify DROP CONSTRAINT PK_umbracoUser2NodeNotify;
+ALTER TABLE [dbo].[umbracoUser2NodeNotify] ALTER COLUMN action char(1) COLLATE database_default NOT NULL;
+ALTER TABLE umbracoUser2NodeNotify ADD CONSTRAINT PK_umbracoUser2NodeNotify PRIMARY KEY CLUSTERED(userId, nodeId, action );
+ALTER TABLE [dbo].[umbracoRelation] ALTER COLUMN comment nvarchar(2000) COLLATE database_default NOT NULL;
+ALTER TABLE [dbo].[cmsDocument] ALTER COLUMN alias nvarchar(510) COLLATE database_default NULL;
---
+++ export-umbraco-data.sql
@@ -0,0 +1,159 @@
+EXECUTE sp_generate_inserts @table_name='cmsContent', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsContentType', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsContentTypeAllowedContentType', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsContentVersion', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsContentXml', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsDataType', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsDataTypePreValues', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsDictionary', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsDocument', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsDocumentType', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsLanguageText', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsMacro', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsMacroProperty', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsMacroPropertyType', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsMember', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsMember2MemberGroup', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsMemberType', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsPropertyData', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsPropertyType', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsStylesheet', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsStylesheetProperty', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsTab', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsTagRelationship', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsTags', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsTask', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsTaskType', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='cmsTemplate', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='dtproperties', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='kpiKeywordsRank', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='kpiWebsite', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='kpiWebsite2', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='local', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='pluginWebstickies', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoApp', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoAppTree', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoDomains', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoLanguage', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoLog', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoNode', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoRelation', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoRelationType', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoStatEntry', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoStatSession', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoStylesheet', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoStylesheetProperty', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoUser', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoUser2app', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoUser2NodeNotify', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoUser2NodePermission', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoUser2userGroup', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoUserGroup', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoUserLogins', @disable_constraints=1;
+GO
+
+EXECUTE sp_generate_inserts @table_name='umbracoUserType', @disable_constraints=1;
+GO
+
